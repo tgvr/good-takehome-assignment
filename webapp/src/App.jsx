2ee1fe50fe19d4a280ad6ef8299763183fe93f0c
Tarun Gavara <tarungavara@gmail.com>
2024-04-09 16:23:22 +0530

    initial webapp

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import Dashboard from './Dashboard';
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Takehome Assignment
-        </Typography>
-      </Box>
-    </Container>
+    <Dashboard />
   );
 }
---
+++ webapp/src/CreateJob.jsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function CreateJob(props) {
+  const { open, toggleModal } = props;
+
+  return (
+    <React.Fragment>
+      <Button color="inherit" variant="outlined" onClick={toggleModal}>
+        Create job
+      </Button>
+      <Dialog
+        open={open}
+        onClose={toggleModal}
+        PaperProps={{
+          component: 'form',
+          onSubmit: async (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+
+            try {
+                const response = await fetch('http://backend:8000/api/create_job', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formJson),
+                });
+
+                if (response.ok) {
+                    const txt = await response.text();
+                    console.log(txt);
+                    toggleModal();
+                } else {
+                    console.error('Error:', response.status);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+          },
+        }}
+      >
+        <DialogTitle>Create new Job</DialogTitle>
+        <DialogContent>
+            <TextField
+                required
+                margin="dense"
+                id="name"
+                name="numFiles"
+                label="Number of files"
+                type="number"
+                fullWidth
+                variant="standard"
+            />
+            <TextField
+                required
+                margin="dense"
+                id="name"
+                name="numValues"
+                label="Number of values per file"
+                type="number"
+                fullWidth
+                variant="standard"
+            />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={toggleModal}>Cancel</Button>
+          <Button type="submit">Create</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
